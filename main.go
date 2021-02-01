024c34c1b6dd7933241e083949749d452ddf6238
davdhng <davidh6472@gmail.com>
2021-02-01 15:53:51 -0500

    surface normals

--- main.go
+++ main.go
@@ -3,15 +3,34 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"os"
 
 	"github.com/cheggaaa/pb"
 )
 
 func rayColor(r Ray) Vec3 {
+	t := hitSphere(Vec3{0, 0, -1}, 0.5, r)
+	if t > 0.0 {
+		N := unit_vector(r.At(t).Sub(Vec3{0, 0, -1}))
+		return Vec3{N.x + 1, N.y + 1, N.z + 1}.Scale(0.5)
+	}
 	unitDirection := unit_vector(r.Direction)
-	t := 0.5 * (unitDirection.y + 1.0)
+	t = 0.5 * (unitDirection.y + 1.0)
 	return Vec3{1.0, 1.0, 1.0}.Scale(1.0 - t).Add(Vec3{0.5, 0.7, 1.0}.Scale(t))
+}
+
+func hitSphere(center Vec3, radius float64, r Ray) float64 {
+	oc := r.Origin.Sub(center)
+	a := r.Direction.Dot(r.Direction)
+	b := oc.Dot(r.Direction) * 2.0
+	c := oc.Dot(oc) - radius*radius
+	discriminant := b*b - 4*a*c
+	if discriminant < 0 {
+		return -1.0
+	} else {
+		return (-b - math.Sqrt(discriminant)) / (2.0 * a)
+	}
 }
 
 func main() {
--- vec3.go
+++ vec3.go
@@ -30,7 +30,7 @@ func (v *Vec3) Mul(v0, v1 *Vec3) {
 	v.z = v0.z * v1.z
 }
 
-func (v *Vec3) Dot(v1 *Vec3) float64 {
+func (v Vec3) Dot(v1 Vec3) float64 {
 	return v.x*v1.x + v.y*v1.y + v.z*v1.z
 }
 
